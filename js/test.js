64918c24391b9824666b2bc1c16ea26acd9e1a06
Olexandr Plakhotniuk <plaxotpos@gmail.com>
2017-03-20 15:55:38 +0200

    test_new
    test_new

---
+++ js/test.js
@@ -0,0 +1,14 @@
+/*jQuery('fm-button').bind('ckick', function() {
+    post =  getFormData();
+
+    console.log(post);
+    alert(2222);
+});*/
+
+/*
+function test (postData) {
+  // post =  getFormData();
+
+    console.log(postData);
+    alert(2222);
+}*/
